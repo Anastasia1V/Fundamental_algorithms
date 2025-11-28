dd9de6f1b1b282a8c836384d34f7d7c08f29c4b1
Anastasia1V <VanastasiaR@yandex.ru>
2025-11-28 16:17:29 +0300

    Арифметические операции

--- 7/src/mathematics.c
+++ 7/src/mathematics.c
@@ -39,8 +39,8 @@ int fast_pow(int base, int power, enum status *st) {
     }
     long long ans = 1;
     while (power) {
-        if (power % 2) {
-            ans *= base;
+        if (power % 2 != 0) {
+            ans = ans * base;
             if (ans > INT_MAX || ans < INT_MIN) {
                 if (st) {
                     *st = MATH_ERROR;
@@ -50,13 +50,14 @@ int fast_pow(int base, int power, enum status *st) {
         }
         power = power / 2;
         if (power) {
-            base *= base;
-            if (base > INT_MAX || base < INT_MIN) {
+            long long base2 = (long long)base * (long long)base;
+            if (base2 > INT_MAX || base2 < INT_MIN) {
                 if (st) {
                     *st = MATH_ERROR;
                 }
                 return 0;
             }
+            base = (int)base2;
         }
     }
     if (st) {
@@ -69,116 +70,302 @@ static void spaces(char *s) {
     if (!s) {
         return;
     }
-    char *start = s;
-    while (*start && isspace((unsigned char)*start)) {
-        start += 1;
+    char *left = s;
+    while (*left && isspace((unsigned char)*left)) {
+        left += 1;
     }
-    char *end = s + strlen(s);
-    while (end > start) {
-        end -= 1;
-        if (!isspace((unsigned char)*end)) {
-            end += 1;
+    char *right = s + strlen(s);
+    while (right > left) {
+        right -= 1;
+        if (!isspace((unsigned char)*right)) {
+            right += 1;
             break;
         }
     }
-    if (end < start) {
+    if (right < left) {
         s[0] = '\0';
         return;
     }
-    size_t length = end - start;
-    memmove(s, start, length);
+    size_t length = right - left;
+    memmove(s, left, length);
     s[length] = '\0';
 }
 
-enum status solve(const char *str, const Variables *vars, int *ans) {
-    if (!str || !ans || !vars) {
-        return INVALID_ARGS;
+static enum status parse(const char *str, const Variables *vars, int *ans) {
+    spaces((char *)str);
+    if (strlen(str) == 0) {
+        return SYNTAX_ERROR;
     }
-    char str_copy[128];
-    strncpy(str_copy, str, sizeof(str_copy) - 1);
-    str_copy[sizeof(str_copy) - 1] = '\0';
-    spaces(str_copy);
-    char *caret = strchr(str_copy, '^');
-    if (caret) {
-        *caret = '\0';
-        char *base_str = str_copy;
-        char *exp_str = caret + 1;
-        spaces(base_str);
-        spaces(exp_str);
-        int base, power;
-        if (strlen(base_str) == 1 && is_correct_name(base_str[0])) {
-            int index = base_str[0] - 'A';
-            if (!vars->initialized[index]) {
-                return NOT_INITIALIZED;
-            }
-            base = vars->values[index];
-        } else if (sscanf(base_str, "%d", &base) != 1) {
-            return SYNTAX_ERROR;
-        }
-        if (strlen(exp_str) == 1 && is_correct_name(exp_str[0])) {
-            int index = exp_str[0] - 'A';
-            if (!vars->initialized[index]) {
-                return NOT_INITIALIZED;
-            }
-            power = vars->values[index];
-        } else if (sscanf(exp_str, "%d", &power) != 1) {
-            return SYNTAX_ERROR;
-        }
-        enum status st;
-        *ans = fast_pow(base, power, &st);
-        return st;
-    }
-    if (strlen(str_copy) == 1 && is_correct_name(str_copy[0])) {
-        int index = str_copy[0] - 'A';
+    if (strlen(str) == 1 && is_correct_name(str[0])) {
+        int index = str[0] - 'A';
         if (!vars->initialized[index]) {
             return NOT_INITIALIZED;
         }
         *ans = vars->values[index];
         return SUCCESS;
     }
-    int value;
-    if (sscanf(str_copy, "%d", &value) == 1) {
-        *ans = value;
+    if (sscanf(str, "%d", ans) == 1) {
         return SUCCESS;
     }
     return SYNTAX_ERROR;
+}
+
+static enum status do_power(const char *str, const Variables *vars, int *ans) {
+    char *caret = strchr(str, '^');
+    if (caret) {
+        *caret = '\0';
+        char *base_str = (char *)str;
+        char *power_str = caret + 1;
+        spaces(base_str);
+        spaces(power_str);
+        int base_int = 0;
+        int power_int = 0;
+        enum status st = parse(base_str, vars, &base_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        st = parse(power_str, vars, &power_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        *ans = fast_pow(base_int, power_int, &st);
+        return st;
+    } else {
+        return parse(str, vars, ans);
+    }
+}
+
+static enum status do_multiply_or_divide(const char *str, const Variables *vars, int *answ) {
+    char *multiply_index = strchr(str, '*');
+    if (multiply_index) {
+        *multiply_index = '\0';
+        char *left_str = (char *)str;
+        char *right_str = multiply_index + 1;
+        spaces(left_str);
+        spaces(right_str);
+        int left_val = 0;
+        int right_val = 0;
+        enum status st = do_power(left_str, vars, &left_val);
+        if (st != SUCCESS) {
+            return st;
+        }
+        st = do_power(right_str, vars, &right_val);
+        if (st != SUCCESS) {
+            return st;
+        }
+        long long ans = (long long)left_val * (long long)right_val;
+        if (ans > INT_MAX || ans < INT_MIN) {
+            return MATH_ERROR;
+        }
+        *answ = (int)ans;
+        return SUCCESS;
+    }
+    char *divide_index = strchr(str, '/');
+    if (divide_index) {
+        *divide_index = '\0';
+        char *left_str = (char *)str;
+        char *right_str = divide_index + 1;
+        spaces(left_str);
+        spaces(right_str);
+        int left_int = 0;
+        int right_int = 0;
+        enum status st = do_power(left_str, vars, &left_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        st = do_power(right_str, vars, &right_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        if (right_int == 0) {
+            return DIVIDE_BY_ZERO;
+        }
+        int ans = left_int / right_int;
+        *answ = ans;
+        return SUCCESS;
+    }
+    return do_power(str, vars, answ);
+}
+
+static enum status do_plus(const char *str, const Variables *vars, int *answ) {
+    char *plus_index = strchr(str, '+');
+    if (plus_index) {
+        *plus_index = '\0';
+        char *left_str = (char *)str;
+        char *right_str = plus_index + 1;
+        spaces(left_str);
+        spaces(right_str);
+        int left_int = 0;
+        int right_int = 0;
+        enum status st = do_multiply_or_divide(left_str, vars, &left_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        st = do_multiply_or_divide(right_str, vars, &right_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        long long ans = (long long)left_int + (long long)right_int;
+        if (ans > INT_MAX || ans < INT_MIN) {
+            return MATH_ERROR;
+        }
+        *answ = (int)ans;
+        return SUCCESS;
+    }
+    return do_multiply_or_divide(str, vars, answ);
+}
+
+static enum status do_minus(const char *str, const Variables *vars, int *answ) {
+    char *minus_index = strchr(str, '-');
+    if (minus_index) {
+        *minus_index = '\0';
+        char *left_str = (char *)str;
+        char *right_str = minus_index + 1;
+        spaces(left_str);
+        spaces(right_str);
+        int left_int = 0;
+        int right_int = 0;
+        enum status st = do_plus(left_str, vars, &left_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        st = do_plus(right_str, vars, &right_int);
+        if (st != SUCCESS) {
+            return st;
+        }
+        long long ans = (long long)left_int - (long long)right_int;
+        if (ans > INT_MAX || ans < INT_MIN) {
+            return MATH_ERROR;
+        }
+        *answ = (int)ans;
+        return SUCCESS;
+    }
+    return do_plus(str, vars, answ);
+}
+
+enum status solve(const char *str, const Variables *vars, int *ans) {
+    if (!str || !ans || !vars) {
+        return INVALID_ARGS;
+    }
+    char copy_str[128];
+    strncpy(copy_str, str, sizeof(copy_str) - 1);
+    copy_str[sizeof(copy_str) - 1] = '\0';
+    spaces(copy_str);
+    return do_minus(copy_str, vars, ans);
+}
+
+static char *find_next_variable_start(char *s, char *end) {
+    char *c = s;
+    while (c < end && *c != '\0') {
+        if (isupper((unsigned char)*c)) {
+            char *next = c + 1;
+            while (next < end && isspace((unsigned char)*next)) {
+                next += 1;
+            }
+            if (next < end && *next == '=') {
+                return c;
+            }
+        }
+        c += 1;
+    }
+    return NULL;
 }
 
 enum status take_command(const char *command, Variables *vars, FILE *file, size_t n) {
     if (!command || !vars) {
         return INVALID_ARGS;
     }
-    char str_copy[128];
-    strncpy(str_copy, command, sizeof(str_copy) - 1);
-    str_copy[sizeof(str_copy) - 1] = '\0';
-    spaces(str_copy);
-    char *equal = strchr(str_copy, '=');
-    if (!equal) {
-        return SYNTAX_ERROR;
-    }
-    *equal = '\0';
-    char *var_str = str_copy;
-    char *expr_str = equal + 1;
-    spaces(var_str);
-    spaces(expr_str);
-    if (strlen(var_str) != 1 || !is_correct_name(var_str[0])) {
-        return SYNTAX_ERROR;
-    }
-    int value;
-    enum status st = solve(expr_str, vars, &value);
-    if (st != SUCCESS) {
-        return st;
-    }
-    int index = var_str[0] - 'A';
-    vars->values[index] = value;
-    vars->initialized[index] = 1;
-    if (file) {
-        fprintf(file, "Line %zu: %s -> %d\n", n, command, value);
+    char copy_str[512];
+    strncpy(copy_str, command, sizeof(copy_str) - 1);
+    copy_str[sizeof(copy_str) - 1] = '\0';
+    spaces(copy_str);
+    char *c = copy_str;
+    char *end_ptr = copy_str + strlen(copy_str);
+    while (1) {
+        while (*c && isspace((unsigned char)*c)) {
+            c += 1;
+        }
+        if (*c == '\0') {
+            break;
+        }
+        if (!isupper((unsigned char)*c)) {
+            char *next_start = find_next_variable_start(c, end_ptr);
+            if (next_start == NULL) {
+                break;
+            } else {
+                c = next_start;
+            }
+        }
+        char *equal = strchr(c, '=');
+        if (!equal) {
+            break;
+        }
+        char *variable_name_index = c;
+        while (variable_name_index < equal && isspace((unsigned char)*variable_name_index)) {
+            variable_name_index += 1;
+        }
+        if (variable_name_index >= equal) {
+            return SYNTAX_ERROR;
+        }
+        char variable_char = *variable_name_index;
+        if (!isupper((unsigned char)variable_char)) {
+            return SYNTAX_ERROR;
+        }
+        char *start_after_equal = equal + 1;
+        char *next_command = find_next_variable_start(start_after_equal, end_ptr);
+        size_t str_length = 0;
+        if (next_command != NULL) {
+            str_length = (size_t)(next_command - start_after_equal);
+        } else {
+            str_length = (size_t)(end_ptr - start_after_equal);
+        }
+        char str[256];
+        if (str_length >= sizeof(str)) {
+            return SYNTAX_ERROR;
+        }
+        strncpy(str, start_after_equal, str_length);
+        str[str_length] = '\0';
+        spaces(str);
+        int value = 0;
+        enum status st = solve(str, vars, &value);
+        if (st != SUCCESS) {
+            if (file) {
+                fprintf(file, "Ошибка на линии %zu: %d\n", n, st);
+            } else {
+                printf("Ошибка на линии %zu: %d\n", n, st);
+            }
+            return st;
+        }
+        int index = variable_char - 'A';
+        vars->values[index] = value;
+        vars->initialized[index] = 1;
+        char line[512];
+        int written = snprintf(line, sizeof(line), "%c = %s", variable_char, str);
+        if (written < 0) {
+            if (file) {
+                fprintf(file, "Ошибка на линии %zu: %d\n", n, SYNTAX_ERROR);
+            } else {
+                printf("Ошибка на линии %zu: %d\n", n, SYNTAX_ERROR);
+            }
+            return SYNTAX_ERROR;
+        }
+        if (file) {
+            fprintf(file, "Строка %zu: %s\n", n, line);
+            fprintf(file, "Результат: %d\n\n", value);
+        } else {
+            printf("Строка %zu: %s\n", n, line);
+            printf("Результат: %d\n\n", value);
+        }
+        if (next_command != NULL) {
+            c = next_command;
+        } else {
+            break;
+        }
     }
     return SUCCESS;
 }
 
-enum status read_file(const char *input, const char *path) {
+enum status read_file(const char *input, const char *output) {
     if (!input) {
         return INVALID_ARGS;
     }
@@ -186,32 +373,37 @@ enum status read_file(const char *input, const char *path) {
     if (!in) {
         return FILE_ERROR;
     }
-    FILE *file = NULL;
-    if (path) {
-        file = fopen(path, "w");
-        if (!file) {
+    FILE *out = NULL;
+    if (output) {
+        out = fopen(output, "w");
+        if (!out) {
             fclose(in);
             return FILE_ERROR;
         }
     }
     Variables vars = {0};
-    char line[256];
+    char str[512];
     size_t n = 1;
-    while (fgets(line, sizeof(line), in)) {
-        size_t len = strlen(line);
-        if (len > 0 && line[len - 1] == '\n') {
-            line[len - 1] = '\0';
+    while (fgets(str, sizeof(str), in)) {
+        size_t length = strlen(str);
+        if (length > 0 && str[length - 1] == '\n') {
+            str[length - 1] = '\0';
+            length -= 1;
         }
-        enum status st = take_command(line, &vars, file, n);
-        if (st != SUCCESS && file) {
-            fprintf(file, "Ошибка на линии %zu: %d\n", n, st);
+        enum status st = take_command(str, &vars, out, n);
+        if (st != SUCCESS) {
+            if (out) {
+                fprintf(out, "Ошибка на линии %zu: %d\n", n, st);
+            } else {
+                printf("Ошибка на линии %zu: %d\n", n, st);
+            }
         }
         n += 1;
     }
     delete_variables(&vars);
     fclose(in);
-    if (file) {
-        fclose(file);
+    if (out) {
+        fclose(out);
     }
     return SUCCESS;
 }
